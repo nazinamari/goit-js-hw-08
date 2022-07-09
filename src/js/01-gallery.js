93796d2c6e86758247ae75b32d91695a7d581920
nazinamari <93985053+nazinamari@users.noreply.github.com>
2022-07-09 17:47:02 +0300

    commit

---
+++ src/js/01-gallery.js
@@ -0,0 +1,38 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+const divRef = document.querySelector('.gallery');
+
+function createGallaryMarkup(items) {
+  return items
+    .map(
+      item =>
+        `<a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" /></a>`
+    )
+    .join('');
+}
+
+const addGallaryMarkup = createGallaryMarkup(galleryItems);
+
+divRef.insertAdjacentHTML('beforeend', addGallaryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
