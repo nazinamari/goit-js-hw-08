65d3b9910f6b30d88334a11e7cec4119240edb5e
nazinamari <93985053+nazinamari@users.noreply.github.com>
2022-07-10 18:02:37 +0300

    08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from "lodash.throttle";
 
+const FEEDBACK = 'feedback-form-state';
+
+let formData = JSON.parse(localStorage.getItem(FEEDBACK)) || {};
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500))
+
+saveFormData();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    if (e.currentTarget.elements.email.value === "" || e.currentTarget.elements.message.value === "") {
+    alert("Please, fill in all the fields!")
+    }
+
+    e.currentTarget.reset();
+
+    localStorage.removeItem(FEEDBACK);
+
+    console.log(formData); 
+ };
+
+function onFormInput(event) {
+    formData[event.target.name] = event.target.value;
+
+    const formInputData = JSON.stringify(formData);
+
+    localStorage.setItem(FEEDBACK, formInputData)
+ };
+
+function saveFormData() {
+    const savedFormData = JSON.parse(localStorage.getItem(FEEDBACK));
+
+    if (savedFormData) {
+        form.elements.email.value = savedFormData.email || "";
+        form.elements.message.value = savedFormData.message || "";
+    }
+}
