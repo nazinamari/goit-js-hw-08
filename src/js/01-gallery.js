1c693632771a72d30e1d33f44e44d02402eaa18a
nazinamari <93985053+nazinamari@users.noreply.github.com>
2022-07-10 17:55:23 +0300

    08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -9,13 +9,7 @@ import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
-console.log(galleryItems);
-
-const divRef = document.querySelector('.gallery');
+const galleryContainer = document.querySelector('.gallery');
 
 function createGallaryMarkup(items) {
   return items
@@ -29,7 +23,7 @@ function createGallaryMarkup(items) {
 
 const addGallaryMarkup = createGallaryMarkup(galleryItems);
 
-divRef.insertAdjacentHTML('beforeend', addGallaryMarkup);
+galleryContainer.insertAdjacentHTML('beforeend', addGallaryMarkup);
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const localStorage = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  localStorage.setItem(localStorage, data.seconds);
+
+  console.log(localStorage.getItem(localStorage));
+};
+
+setCurrentTime();
+
+function setCurrentTime() {
+  if (!localStorage.getItem(localStorage)) {
+    return;
+  }
+  player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
