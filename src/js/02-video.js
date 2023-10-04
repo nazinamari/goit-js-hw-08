68d57fdea4f16a59f5f53740cba15264a4d0a9c8
nazinamari <93985053+nazinamari@users.noreply.github.com>
2023-10-04 15:19:55 +0100

    change code

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,26 +2,31 @@ import Player from '@vimeo/player';
 
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('#vimeo-player');
-const player = new Player(iframe);
+const player = new Player(document.querySelector('#vimeo-player'));
+const VIDEO_CURR_TIME = 'videoplayer-current-time';
 
-const localStorage = 'videoplayer-current-time';
+if (localStorage.getItem(VIDEO_CURR_TIME)) {
+  player
+    .setCurrentTime(JSON.parse(localStorage.getItem(VIDEO_CURR_TIME)))
+    .catch(error => {
+      switch (error.name) {
+        case 'RangeError':
+          console.error(
+            'The time was less than 0 or greater than the video’s duration.'
+          );
+          player.setCurrentTime(0);
+          break;
+        
+        default:
+          console.error(error.name);
+          break;
+      }
+  })
+}
 
-console.log(localStorage);
-
-// const onPlay = function (data) {
-//   localStorage.setItem(localStorage, data.seconds);
-
-//   console.log(localStorage.getItem(localStorage));
-// };
-
-// setCurrentTime();
-
-// function setCurrentTime() {
-//   if (!localStorage.getItem(localStorage)) {
-//     return;
-//   }
-//   player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
-// }
-
-// player.on('timeupdate', throttle(onPlay, 1000));
+player.on(
+  'timeupdate',
+  throttle(e => {
+    localStorage.setItem(VIDEO_CURR_TIME, e.seconds);
+  }, 1000)
+);
